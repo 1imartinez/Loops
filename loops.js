c1ae0618a18213fa98fd78d63acab5409f851a5b
1imartinez <42749604+1imartinez@users.noreply.github.com>
2018-10-16 13:56:51 -0500

    Loops are hard

---
+++ loops.js
@@ -0,0 +1,31 @@
+// For-loop functions
+let sum=0;
+let n=100;
+let i=1
+for(i <-n ){
+sum=sum+1;
+i++;
+
+
+}
+
+
+function additionPatternSum(/* Give it three inputs */){
+
+}
+
+
+
+
+// While-loop function
+
+function multiplicationPatternSum(/* Give it three inputs */){
+
+
+}
+
+
+
+
+
+//Later copy paste the checking code below here.
