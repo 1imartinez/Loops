d855d6a83799b7d862cb94c4e7a6584c015a1b25
1imartinez <42749604+1imartinez@users.noreply.github.com>
2018-10-19 13:58:22 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -11,10 +11,10 @@ function possitiveIntSum(limit){
 
 function additionPatternSum(start, diff, limit){
       let sum= start;
-      for(let k= 0;k<limit; k++){
-      sum= sum+diff*k;
+      for(let k= 0;k<= limit; k++){
+            sum= sum+diff*k;
       }
-
+      return sum;
 }
 
 
@@ -23,41 +23,16 @@ function additionPatternSum(start, diff, limit){
 // While-loop function
 
 function multiplicationPatternSum(start,factor,max){
-let sum= start;
-while(let k= 0; k<max; k++;{
-sum=sum+factor*k;
+      let sum= start;
+      let i= start;
+      while(i<=max){
+            sum=sum + i*factor;
+            i= i*factor;
+      } 
+      return sum;
 }
 
 
 
-
-
-
-//Later copy paste the checking code below here.
-//Declarion of checking code
-function check(){
-      let smallLimit = 208;
-      let mediumLimit = 1024;
-      let largeLimit = 600000;//six hundred thousand
-  
-      let test1 = 21736 === positiveIntSum(smallLimit);
-      let test2 = 180000300000 === positiveIntSum(mediumLimit);
-      let test3 = 1770 === additionPatternSum(60,3,20);
-      let test4 = 8375 === additionPatternSum(-200, 15, 50);
-      let test5 = 12345678 === multiplicationPatternSum(1,3,largeLimit);
-      let test6 = 12345678 === multiplicationPatternSum(5,2,largeLimit);
-      let test7 = 12345678 === multiplicationPatternSum(2,-7,largeLimit);
-  
-      console.log(`The function positiveIntSum works properly: ${test1 && test2}`);
-      console.log(`The function additionPatternSum works properly: ${test3 && test4}`);
-      console.log(`The function positiveIntSum works properly: ${test5 && test6 && test7}`);
-      console.log(`-------------------------------------------------------------`);
-      console.log(`
-      Common mistakes to check
-      1. Did you spell everything correctly?
-      2. Does it actually return an answer?
-      3. Were your original notes correct?
-      `);
-  }
-  //Actually running the checking code.
-  check();
+console.log("q1: "+possitiveIntSum(1500));
+console.log("q2: "+possitiveIntSum(2000000))
